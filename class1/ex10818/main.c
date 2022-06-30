eb6c9bb841be01f64a8429c50e9ecc853b60e630
eunwoo <lkjin0110@gmail.com>
2022-06-30 17:19:12 +0900

    submit 10818

---
+++ class1/ex10818/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, max, min, num;
+    max = -1000000;
+    min = 1000000;
+
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++)
+    {
+        scanf("%d", &num);
+        if(num > max)
+        {
+            max = num;
+        }
+        if(num < min)
+        {
+            min = num;
+        }
+    }
+    printf("%d %d", min, max);
+    return 0;
+}
