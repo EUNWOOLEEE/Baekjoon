8a72955ab8330f5a9b822b6ec733ee8a46ccb65e
eunwoo <lkjin0110@gmail.com>
2022-07-02 17:32:17 +0900

    submit 1085

---
+++ class2/ex1085/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int X, Y, W, H;
+    int outX, outY;
+
+    scanf("%d", &X);
+    scanf("%d", &Y);
+    scanf("%d", &W);
+    scanf("%d", &H);
+
+    if(X < W / 2)
+        outX = X;
+    else if(X > W /2)
+        outX = W - X;
+        
+    if(Y < H / 2)
+        outY = Y;
+    else if(Y > H / 2)
+        outY = H - Y;
+
+    if(outX >= outY)
+        printf("%d", outY);
+    else if(outX < outY)
+        printf("%d", outX);
+    return 0;
+}
