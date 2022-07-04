3f07b2e184b7832691dc42436bf205550faf7622
eunwoo <lkjin0110@gmail.com>
2022-07-04 20:16:29 +0900

    main modify

--- class2/ex10250/main.c
+++ class2/ex10250/main.c
@@ -8,27 +8,21 @@ int main()
     while (T)
     {
         scanf("%d %d %d", &h, &w, &n);
-        floor = room = 1;
-        if (n == 1)
-        {
-            printf("%d0%d\n", floor, room);
-            return 0;
-        }
+        floor = room = 0;
 
         for (int x = 0; x < w; x++)
         {
-            for (int y = 1; y < h; y++)
+            for (int y = 0; y < h; y++)
             {
                 n--;
-                if (n <= 0)
-                    break;
                 floor++;
+                if (n == 0)
+                    break;
             }
-            n--;
-            if (n <= 0)
-                break;
-            floor = 1;
             room++;
+            if (n == 0)
+                break;
+            floor = 0;
         }
         printf("%d", floor);
         if (room < 10)
