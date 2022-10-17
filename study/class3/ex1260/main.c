db97926017cb0db8dda85ecd20d34b40016eeb11
eunwoo <lkjin0110@gmail.com>
2022-10-17 11:07:30 +0900

    clear 삭제

--- study/class3/ex1260/main.c
+++ study/class3/ex1260/main.c
@@ -6,22 +6,14 @@ int visited[MAX] = {0,};
 int queue[MAX] = {0,};
 int link[MAX][MAX] = {0,};
 
-void clear(int n)
-{
-	for(int i = 0; i <= n; i++)
-		visited[i] = 0;
-}
-
 void dfs(int num)
 {
 	visited[num]++;
 	printf("%d ", num);
 
 	for(int i = 0; i <= n; i++)
-	{
 		if(link[num][i] && !visited[i])
 			dfs(i);
-	}
 }
 
 void bfs(int num)
@@ -37,14 +29,12 @@ void bfs(int num)
 		next = queue[front++];
 
 		for(int i = 0; i <= n; i++)
-		{
 			if(link[next][i] && !visited[i])
 			{
 				printf("%d ", i);
 				queue[rear++] = i;
 				visited[i]++;
 			}
-		}
 	}
 	next = queue[front++];
 }
@@ -65,8 +55,9 @@ int main()
 	dfs(v);
 
 	printf("\n");
-	clear(n);
-	
+	for(int i = 0; i <= n; i++)
+		visited[i] = 0;
+
 	bfs(v);
 
 	return 0;
